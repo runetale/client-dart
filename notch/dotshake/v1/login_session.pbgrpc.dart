11484acf5939d25f11da9d54af45624201433272
GitHub Action <action@github.com>
2023-04-01 08:22:31 +0000

    Sync to proto commit
    Notch-Technologies/proto@16655e9eef318ff68482bb3f2d81a74178cb0c73

--- notch/dotshake/v1/login_session.pbgrpc.dart
+++ notch/dotshake/v1/login_session.pbgrpc.dart
@@ -11,23 +11,23 @@ import 'dart:core' as $core;
 
 import 'package:grpc/service_api.dart' as $grpc;
 import '../../../google/protobuf/empty.pb.dart' as $0;
-import 'login_session.pb.dart' as $5;
+import 'login_session.pb.dart' as $4;
 export 'login_session.pb.dart';
 
 class LoginSessionServiceClient extends $grpc.Client {
   static final _$streamPeerLoginSession =
-      $grpc.ClientMethod<$0.Empty, $5.PeerLoginSessionResponse>(
+      $grpc.ClientMethod<$0.Empty, $4.PeerLoginSessionResponse>(
           '/protos.LoginSessionService/StreamPeerLoginSession',
           ($0.Empty value) => value.writeToBuffer(),
           ($core.List<$core.int> value) =>
-              $5.PeerLoginSessionResponse.fromBuffer(value));
+              $4.PeerLoginSessionResponse.fromBuffer(value));
 
   LoginSessionServiceClient($grpc.ClientChannel channel,
       {$grpc.CallOptions? options,
       $core.Iterable<$grpc.ClientInterceptor>? interceptors})
       : super(channel, options: options, interceptors: interceptors);
 
-  $grpc.ResponseStream<$5.PeerLoginSessionResponse> streamPeerLoginSession(
+  $grpc.ResponseStream<$4.PeerLoginSessionResponse> streamPeerLoginSession(
       $async.Stream<$0.Empty> request,
       {$grpc.CallOptions? options}) {
     return $createStreamingCall(_$streamPeerLoginSession, request,
@@ -39,15 +39,15 @@ abstract class LoginSessionServiceBase extends $grpc.Service {
   $core.String get $name => 'protos.LoginSessionService';
 
   LoginSessionServiceBase() {
-    $addMethod($grpc.ServiceMethod<$0.Empty, $5.PeerLoginSessionResponse>(
+    $addMethod($grpc.ServiceMethod<$0.Empty, $4.PeerLoginSessionResponse>(
         'StreamPeerLoginSession',
         streamPeerLoginSession,
         true,
         true,
         ($core.List<$core.int> value) => $0.Empty.fromBuffer(value),
-        ($5.PeerLoginSessionResponse value) => value.writeToBuffer()));
+        ($4.PeerLoginSessionResponse value) => value.writeToBuffer()));
   }
 
-  $async.Stream<$5.PeerLoginSessionResponse> streamPeerLoginSession(
+  $async.Stream<$4.PeerLoginSessionResponse> streamPeerLoginSession(
       $grpc.ServiceCall call, $async.Stream<$0.Empty> request);
 }
