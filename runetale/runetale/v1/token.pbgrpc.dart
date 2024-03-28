64f3e25fb8e260882c07ea25e5634bce7be89a4a
GitHub Action <action@github.com>
2024-03-28 06:44:44 +0000

    Sync to proto commit
    runetale/proto@7f2283726d25de9ba73beebb25660b5dac209337

---
+++ runetale/runetale/v1/token.pbgrpc.dart
@@ -0,0 +1,59 @@
+//
+//  Generated code. Do not modify.
+//  source: runetale/runetale/v1/token.proto
+//
+// @dart = 2.12
+
+// ignore_for_file: annotate_overrides, camel_case_types, comment_references
+// ignore_for_file: constant_identifier_names, library_prefixes
+// ignore_for_file: non_constant_identifier_names, prefer_final_fields
+// ignore_for_file: unnecessary_import, unnecessary_this, unused_import
+
+import 'dart:async' as $async;
+import 'dart:core' as $core;
+
+import 'package:grpc/service_api.dart' as $grpc;
+import 'package:protobuf/protobuf.dart' as $pb;
+
+import '../../../google/protobuf/empty.pb.dart' as $1;
+
+export 'token.pb.dart';
+
+@$pb.GrpcServiceName('protos.TokenService')
+class TokenServiceClient extends $grpc.Client {
+  static final _$refreshToken = $grpc.ClientMethod<$1.Empty, $1.Empty>(
+      '/protos.TokenService/RefreshToken',
+      ($1.Empty value) => value.writeToBuffer(),
+      ($core.List<$core.int> value) => $1.Empty.fromBuffer(value));
+
+  TokenServiceClient($grpc.ClientChannel channel,
+      {$grpc.CallOptions? options,
+      $core.Iterable<$grpc.ClientInterceptor>? interceptors})
+      : super(channel, options: options,
+        interceptors: interceptors);
+
+  $grpc.ResponseFuture<$1.Empty> refreshToken($1.Empty request, {$grpc.CallOptions? options}) {
+    return $createUnaryCall(_$refreshToken, request, options: options);
+  }
+}
+
+@$pb.GrpcServiceName('protos.TokenService')
+abstract class TokenServiceBase extends $grpc.Service {
+  $core.String get $name => 'protos.TokenService';
+
+  TokenServiceBase() {
+    $addMethod($grpc.ServiceMethod<$1.Empty, $1.Empty>(
+        'RefreshToken',
+        refreshToken_Pre,
+        false,
+        false,
+        ($core.List<$core.int> value) => $1.Empty.fromBuffer(value),
+        ($1.Empty value) => value.writeToBuffer()));
+  }
+
+  $async.Future<$1.Empty> refreshToken_Pre($grpc.ServiceCall call, $async.Future<$1.Empty> request) async {
+    return refreshToken(call, await request);
+  }
+
+  $async.Future<$1.Empty> refreshToken($grpc.ServiceCall call, $1.Empty request);
+}
