a5dea9e53e67fb411583f28826e9fabffc2178cd
GitHub Action <action@github.com>
2024-10-02 07:20:49 +0000

    Sync to proto commit
    runetale/proto@7d313170c81770a679b09319947a02fb94111dc3

--- runetale/runetale/v1/node.pbgrpc.dart
+++ runetale/runetale/v1/node.pbgrpc.dart
@@ -22,10 +22,6 @@ export 'node.pb.dart';
 
 @$pb.GrpcServiceName('protos.NodeService')
 class NodeServiceClient extends $grpc.Client {
-  static final _$syncRemoteNodesConfig = $grpc.ClientMethod<$0.Empty, $6.SyncNodesResponse>(
-      '/protos.NodeService/SyncRemoteNodesConfig',
-      ($0.Empty value) => value.writeToBuffer(),
-      ($core.List<$core.int> value) => $6.SyncNodesResponse.fromBuffer(value));
   static final _$composeNode = $grpc.ClientMethod<$0.Empty, $6.ComposeNodeResponse>(
       '/protos.NodeService/ComposeNode',
       ($0.Empty value) => value.writeToBuffer(),
@@ -41,16 +37,12 @@ class NodeServiceClient extends $grpc.Client {
       : super(channel, options: options,
         interceptors: interceptors);
 
-  $grpc.ResponseFuture<$6.SyncNodesResponse> syncRemoteNodesConfig($0.Empty request, {$grpc.CallOptions? options}) {
-    return $createUnaryCall(_$syncRemoteNodesConfig, request, options: options);
-  }
-
   $grpc.ResponseFuture<$6.ComposeNodeResponse> composeNode($0.Empty request, {$grpc.CallOptions? options}) {
     return $createUnaryCall(_$composeNode, request, options: options);
   }
 
-  $grpc.ResponseFuture<$6.NetworkMapResponse> getNetworkMap($0.Empty request, {$grpc.CallOptions? options}) {
-    return $createUnaryCall(_$getNetworkMap, request, options: options);
+  $grpc.ResponseStream<$6.NetworkMapResponse> getNetworkMap($async.Stream<$0.Empty> request, {$grpc.CallOptions? options}) {
+    return $createStreamingCall(_$getNetworkMap, request, options: options);
   }
 }
 
@@ -59,13 +51,6 @@ abstract class NodeServiceBase extends $grpc.Service {
   $core.String get $name => 'protos.NodeService';
 
   NodeServiceBase() {
-    $addMethod($grpc.ServiceMethod<$0.Empty, $6.SyncNodesResponse>(
-        'SyncRemoteNodesConfig',
-        syncRemoteNodesConfig_Pre,
-        false,
-        false,
-        ($core.List<$core.int> value) => $0.Empty.fromBuffer(value),
-        ($6.SyncNodesResponse value) => value.writeToBuffer()));
     $addMethod($grpc.ServiceMethod<$0.Empty, $6.ComposeNodeResponse>(
         'ComposeNode',
         composeNode_Pre,
@@ -75,26 +60,17 @@ abstract class NodeServiceBase extends $grpc.Service {
         ($6.ComposeNodeResponse value) => value.writeToBuffer()));
     $addMethod($grpc.ServiceMethod<$0.Empty, $6.NetworkMapResponse>(
         'GetNetworkMap',
-        getNetworkMap_Pre,
-        false,
-        false,
+        getNetworkMap,
+        true,
+        true,
         ($core.List<$core.int> value) => $0.Empty.fromBuffer(value),
         ($6.NetworkMapResponse value) => value.writeToBuffer()));
-  }
-
-  $async.Future<$6.SyncNodesResponse> syncRemoteNodesConfig_Pre($grpc.ServiceCall call, $async.Future<$0.Empty> request) async {
-    return syncRemoteNodesConfig(call, await request);
   }
 
   $async.Future<$6.ComposeNodeResponse> composeNode_Pre($grpc.ServiceCall call, $async.Future<$0.Empty> request) async {
     return composeNode(call, await request);
   }
 
-  $async.Future<$6.NetworkMapResponse> getNetworkMap_Pre($grpc.ServiceCall call, $async.Future<$0.Empty> request) async {
-    return getNetworkMap(call, await request);
-  }
-
-  $async.Future<$6.SyncNodesResponse> syncRemoteNodesConfig($grpc.ServiceCall call, $0.Empty request);
   $async.Future<$6.ComposeNodeResponse> composeNode($grpc.ServiceCall call, $0.Empty request);
-  $async.Future<$6.NetworkMapResponse> getNetworkMap($grpc.ServiceCall call, $0.Empty request);
+  $async.Stream<$6.NetworkMapResponse> getNetworkMap($grpc.ServiceCall call, $async.Stream<$0.Empty> request);
 }
